fd8f3ac11d246a00909266cbb439bec557cf55be
V <30593374+Hagi3l@users.noreply.github.com>
2020-07-17 15:53:58 +1000

    Complete Task5

---
+++ Labs/week1/task5/index.js
@@ -0,0 +1,11 @@
+function multiplyNums(x, y, z){
+    return "new number is " + x * y * z;
+}
+
+console.log(multiplyNums(1, 2, 3));
+
+multiplyNumsArray = (x, y, z) => {
+    return "new number is " + x * y * z;
+}
+
+console.log(multiplyNumsArray(1,2,4));
