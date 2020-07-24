fe6bf6e1f2c7656884e9f973edd8c1c2e8698630
V <30593374+Hagi3l@users.noreply.github.com>
2020-07-24 14:28:18 +1000

    Update index.js

--- Labs/week2/task2/index.js
+++ Labs/week2/task2/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //task2
 console.log("Hello World!");
 
@@ -10,8 +9,3 @@ adder = (x, y) => {
 };
 
 console.log(adder(5, 10));
-=======
-console.log("Hello World!");
-
-//Adding change to initiate merge conflict.
->>>>>>> 7c8ee7ce275a77e65253c17f7ca28c5f028b35dd
