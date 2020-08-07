79c6e3df348cd671d38ec4ba6816d62881bffd17
V <30593374+Hagi3l@users.noreply.github.com>
2020-08-07 13:53:28 +1000

    Integrated user login
    - Added ngModel to email and password
    - Added click event to the login button
    - Added a temp database of potential users
    - Added auth when logging in

--- Labs/week4/week4/src/app/login/login.component.ts
+++ Labs/week4/week4/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = "";
+  password = "";
+
+  database = [
+    {
+        email: "jon@gmail.com",
+        password: "123"
+    },
+    {
+        email: "james@gmail.com",
+        password: "321"
+    },
+    {
+        email: "tony@gmail.com",
+        password: "432"
+    }
+];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  itemClicked() {
+    if(this.auth = this.database.some(item => this.email == item.email && this.password == item.password)) {
+      this.router.navigateByUrl('/account');
+    }
+
   }
 
 }
