8b4923b3299452e7c77d0350f264c1510fa36e5d
V <30593374+Hagi3l@users.noreply.github.com>
2020-08-28 04:08:59 +1000

    Update app.module.ts

--- Labs/week6/chat/src/app/app.module.ts
+++ Labs/week6/chat/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatInterfaceComponent } from './chat-interface/chat-interface.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatInterfaceComponent
   ],
   imports: [
     BrowserModule,
