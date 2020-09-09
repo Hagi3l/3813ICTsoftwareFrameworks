189c717b50bf3da60d90b184b7d891f3b1203db5
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-09 20:47:18 +1000

    Chat component
    Modified init files with custom built

---
+++ Assignment/chat-app/src/app/chat/chat/chat.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../services/socket.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  newMessage:string = "";
+  messages:string[] = [];
+  ioConnection:any;
+
+  constructor(private socketService:SocketService) { }
+
+  ngOnInit() {
+    this.initIoConnection();
+  }
+
+  private initIoConnection() {
+    this.socketService.initSocket();
+    this.ioConnection = this.socketService.onMessage().subscribe((message:string) => {
+      this.messages.push(message);
+    });
+  }
+
+  public chat() {
+
+    if (this.newMessage) {
+      this.socketService.send(this.newMessage);
+      this.newMessage=null;
+    } else {
+      console.log('No Message');
+    }
+  }
+}
