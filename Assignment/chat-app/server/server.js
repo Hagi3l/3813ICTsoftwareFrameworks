b7a8ae8977ecf8fc834cf16394fade2359245723
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-10 11:18:33 +1000

    Update server.js
    Created groups and channels api

--- Assignment/chat-app/server/server.js
+++ Assignment/chat-app/server/server.js
@@ -7,13 +7,15 @@ const sockets = require('./socket.js');
 const server = require('./listen.js');
 
 const PORT = 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.post('/api/login-auth', require('./router/login'));
 
 sockets.connect(io, PORT);
-
 server.listen(http, PORT);
 
 
+app.post('/api/login-auth', require('./router/login'));
+app.post('/api/groups', require('./router/groups.js'));
+app.post('/api/channels', require('./router/channels.js'));
