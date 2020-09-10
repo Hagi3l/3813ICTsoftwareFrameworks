d835df6efa5a5c817ddea06313bd999f84e9544d
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-10 11:17:59 +1000

    Create channels.js
    Created channels

---
+++ Assignment/chat-app/server/router/channels.js
@@ -0,0 +1,51 @@
+module.exports = function(req, res) {
+
+  let channels = [
+    {
+      'id': 1,
+      'channel_name': 'Private',
+      'group_id': 1,
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 2,
+      'channel_name': 'join me',
+      'group_id': 1,
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 3,
+      'channel_name': 'dammmm',
+      'group_id': 1,
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 4,
+      'channel_name': 'wow',
+      'group_id': 1,
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 5,
+      'channel_name': 'quick Club',
+      'group_id': 2,
+      'users': [
+        'super'
+      ]
+    },
+  ];
+
+  console.log(req);
+  res.send(channels);
+
+};
+
+
