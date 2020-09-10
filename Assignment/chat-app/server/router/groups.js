9b232390e644c4b85a506a86004234cb11a09e23
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-10 11:18:08 +1000

    Create groups.js
    Created groups

---
+++ Assignment/chat-app/server/router/groups.js
@@ -0,0 +1,46 @@
+module.exports = function(req, res) {
+
+  let groups = [
+    {
+      'id': 1,
+      'group_name': 'Elite',
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 2,
+      'group_name': 'Fun',
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 3,
+      'group_name': 'Europe',
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 4,
+      'group_name': 'Australia',
+      'users': [
+        'super'
+      ]
+    },
+    {
+      'id': 5,
+      'group_name': 'Motorcycle Club',
+      'users': [
+        'super'
+      ]
+    },
+  ];
+
+  console.log(req);
+  res.send(groups);
+
+};
+
+
