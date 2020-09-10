a5701e735413631dd260698569853989fc895949
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-10 14:50:42 +1000

    Updated group functionality
    Converted from array to json local storage

--- Assignment/chat-app/server/router/groups.js
+++ Assignment/chat-app/server/router/groups.js
@@ -1,45 +1,19 @@
 module.exports = function(req, res) {
 
-  let groups = [
-    {
-      'id': 1,
-      'group_name': 'Elite',
-      'users': [
-        'super',
-        'test'
-      ]
-    },
-    {
-      'id': 2,
-      'group_name': 'Fun',
-      'users': [
-        'super'
-      ]
-    },
-    {
-      'id': 3,
-      'group_name': 'Europe',
-      'users': [
-        'super'
-      ]
-    },
-    {
-      'id': 4,
-      'group_name': 'Australia',
-      'users': [
-        'super'
-      ]
-    },
-    {
-      'id': 5,
-      'group_name': 'Motorcycle Club',
-      'users': [
-        'super'
-      ]
-    },
-  ];
+    const fs = require('fs');
+    let ok = false;
 
-  res.send(groups);
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
+
+    fs.readFile('./data/channels_groups.json', (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            res.send(JSON.parse(data).groups);
+        }
+    });
 
 };
 
--- Assignment/chat-app/src/app/chat/chat.component.ts
+++ Assignment/chat-app/src/app/chat/chat.component.ts
@@ -5,122 +5,122 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const BACKEND_URL = 'http://localhost:3000';
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
 };
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
 
-  newMessage:String;
-  messages:Array<{username:String, message:String}> = [];
-  groups:Array<{id:Number, group_name:String, users:Array<String>}> = [];
-  group:Array<{id:Number, group_name:String, users:Array<String>}>;
-  channels:Array<{id:Number, channel_name:String, group_id:Number, users:Array<String>}> = [];
-  channel:String;
-  ioConnection:any;
+    newMessage:String;
+    messages:Array<{username:String, message:String}> = [];
+    groups:Array<{id:Number, group_name:String, users:Array<String>}> = [];
+    group:Array<{id:Number, group_name:String, users:Array<String>}>;
+    channels:Array<{id:Number, channel_name:String, group_id:Number, users:Array<String>}> = [];
+    channel:String;
+    ioConnection:any;
 
-  username = localStorage.getItem('username');
-  session: boolean;
+    username = localStorage.getItem('username');
+    session: boolean;
 
-  constructor(private socketService:SocketService, private router: Router, private httpClient: HttpClient) { }
+    constructor(private socketService:SocketService, private router: Router, private httpClient: HttpClient) { }
 
-  ngOnInit() {
-    this.getGroups();
-    this.initIoConnection();
-  }
-
-  private initIoConnection() {
-    this.socketService.initSocket();
-    this.socketService.onMessage()
-    .subscribe(data => this.messages.push(data));
-
-    this.socketService.newUserJoined()
-    .subscribe(data => this.messages.push(data));
-
-    this.socketService.userLeftRoom()
-    .subscribe(data => this.messages.push(data));
-  }
-
-  private getGroups() {
-
-    this.httpClient.post(BACKEND_URL + '/api/groups', httpOptions)
-    .subscribe((data: any) => {
-      if (data) {
-        data.forEach(g => {
-          g.users.forEach(user => {
-            if (user == this.username) {
-              this.groups.push(g);
-              console.log(this.groups);
-            }
-          });
-        });
-
-        // sessionStorage.setItem('id', data.id.toString());
-        // sessionStorage.setItem('username', data.username.toString());
-        // localStorage.setItem('id', data.id.toString());
-        // localStorage.setItem('username', data.username.toString());
-        // this.httpClient.post(BACKEND_URL + '/api/login-success', data, httpOptions)
-        // .subscribe((m: any) => {});
-        // this.router.navigateByUrl('chat');
-      } else {
-        alert('Sorry, invalid username or password');
-      }
-
-    });
-  }
-
-  private getChannnels(group_id) {
-
-
-    this.httpClient.post(BACKEND_URL + '/api/channels', httpOptions)
-    .subscribe((data: any) => {
-      if (data) {
-        data.forEach(c => {
-          if (c.group_id == group_id) {
-            console.log('found a channel in the group');
-            console.log(c.channel_name);
-            this.channels.push(c);
-          }
-        });
-        // sessionStorage.setItem('id', data.id.toString());
-        // sessionStorage.setItem('username', data.username.toString());
-        // localStorage.setItem('id', data.id.toString());
-        // localStorage.setItem('username', data.username.toString());
-        // this.httpClient.post(BACKEND_URL + '/api/login-success', data, httpOptions)
-        // .subscribe((m: any) => {});
-        // this.router.navigateByUrl('chat');
-      } else {
-        alert('Sorry, invalid username or password');
-      }
-
-    });
-  }
-
-  public chat() {
-    if (this.newMessage) {
-      this.socketService.send({username: this.username, room: this.channel, message: this.newMessage});
-      this.newMessage = '';
-      //localStorage.setItem('messages', this.messages)
-    } else {
-      console.log('No Message');
+    ngOnInit() {
+        this.getGroups();
+        this.initIoConnection();
     }
-  }
 
-  public join() {
-    this.socketService.joinRoom({username: this.username, room: this.channel});
-  }
+    private initIoConnection() {
+        this.socketService.initSocket();
+        this.socketService.onMessage()
+        .subscribe(data => this.messages.push(data));
 
-  public leave() {
-    this.socketService.leaveRoom({username: this.username, room: this.channel});
-  }
+        this.socketService.newUserJoined()
+        .subscribe(data => this.messages.push(data));
 
-  public groupSelected() {
-    this.channels = [];
-    this.getChannnels(this.group);
+        this.socketService.userLeftRoom()
+        .subscribe(data => this.messages.push(data));
+    }
 
-  }
+    private getGroups() {
+
+        this.httpClient.post(BACKEND_URL + '/api/groups', httpOptions)
+        .subscribe((data: any) => {
+            if (data) {
+                data.forEach(g => {
+                    g.users.forEach(user => {
+                        if (user == this.username) {
+                            this.groups.push(g);
+                            console.log(this.groups);
+                        }
+                    });
+                });
+
+                // sessionStorage.setItem('id', data.id.toString());
+                // sessionStorage.setItem('username', data.username.toString());
+                // localStorage.setItem('id', data.id.toString());
+                // localStorage.setItem('username', data.username.toString());
+                // this.httpClient.post(BACKEND_URL + '/api/login-success', data, httpOptions)
+                // .subscribe((m: any) => {});
+                // this.router.navigateByUrl('chat');
+            } else {
+                alert('Sorry, invalid username or password');
+            }
+
+        });
+    }
+
+    private getChannnels(group_id) {
+
+
+        this.httpClient.post(BACKEND_URL + '/api/channels', httpOptions)
+        .subscribe((data: any) => {
+            if (data) {
+                data.forEach(c => {
+                    if (c.group_id == group_id) {
+                        console.log('found a channel in the group');
+                        console.log(c.channel_name);
+                        this.channels.push(c);
+                    }
+                });
+                // sessionStorage.setItem('id', data.id.toString());
+                // sessionStorage.setItem('username', data.username.toString());
+                // localStorage.setItem('id', data.id.toString());
+                // localStorage.setItem('username', data.username.toString());
+                // this.httpClient.post(BACKEND_URL + '/api/login-success', data, httpOptions)
+                // .subscribe((m: any) => {});
+                // this.router.navigateByUrl('chat');
+            } else {
+                alert('Sorry, invalid username or password');
+            }
+
+        });
+    }
+
+    public chat() {
+        if (this.newMessage) {
+            this.socketService.send({username: this.username, room: this.channel, message: this.newMessage});
+            this.newMessage = '';
+            //localStorage.setItem('messages', this.messages)
+        } else {
+            console.log('No Message');
+        }
+    }
+
+    public join() {
+        this.socketService.joinRoom({username: this.username, room: this.channel});
+    }
+
+    public leave() {
+        this.socketService.leaveRoom({username: this.username, room: this.channel});
+    }
+
+    public groupSelected() {
+        this.channels = [];
+        this.getChannnels(this.group);
+
+    }
 }
