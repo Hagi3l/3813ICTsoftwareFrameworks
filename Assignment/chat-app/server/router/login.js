c47c2b795893c73bca980f357118475d37a42274
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-10 14:53:45 +1000

    Update login.js

--- Assignment/chat-app/server/router/login.js
+++ Assignment/chat-app/server/router/login.js
@@ -23,20 +23,4 @@ module.exports = function(req, res) {
           return res.send(ok);
         }
     });
-
-
-
-
-
-
-    // let userIndex = users.findIndex(user => ((user.username == req.body.username) && (user.password == req.body.password)));
-
-    // if (userIndex == -1) {
-    // console.log('cant find the user');
-    // res.send({'ok': false});
-    // } else {
-    // users[userIndex].ok = true;
-    // let user = users[userIndex];
-    // res.send(user);
-    // }
 };
