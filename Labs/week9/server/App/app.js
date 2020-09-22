016eabd26f4a46b3325cda23bed6158ba7457308
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-22 23:55:22 +1000

    Added 4 new files

---
+++ Labs/week9/server/App/app.js
@@ -0,0 +1,22 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'mydb';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.strictEqual(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+const insertProducts = function(db, callback) {
+  const collection = db.collection('products');
+}
