e8115404c9daa7f01ec21ebc5ad17c32c970e737
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-24 00:35:02 +1000

    Update app.js

--- Labs/week9/backend/App/app.js
+++ Labs/week9/backend/App/app.js
@@ -6,6 +6,30 @@ const read = require('./read');
 const remove = require('./remove');
 const update = require('./update');
 
+const docArray = [
+  {
+      "id": "1",
+      "name": "iPhone 6",
+      "description": "Best phone in the world!",
+      "price": 999,
+      "units": 99
+  },
+  {
+      "id": "2",
+      "name": "iPhone X",
+      "description": "Not a bad phone",
+      "price": 1499,
+      "units": 25
+  },
+  {
+      "id": "3",
+      "name": "Samsung Galaxy S10",
+      "description": "Number 1 phone on the planet",
+      "price": 850,
+      "units": 75
+  }
+];
+
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -21,6 +45,8 @@ const client = MongoClient.connect(url, function(err, client) {
   const db = client.db(dbName);
 
   const collection = db.collection('products');
+
+  add(collection, docArray);
 
   client.close();
 });
