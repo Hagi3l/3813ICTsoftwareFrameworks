f42c1369b11c625ea11a2ef217ba6cbaf8ceaa70
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-24 01:00:39 +1000

    added products class and product-data service

--- Labs/week9/frontend/src/app/product-data.service.ts
+++ Labs/week9/frontend/src/app/product-data.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Products } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductDataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  add(product:Products) {
+    return this.http.post<any>('http://localhost:3000/api/add', product);
+  }
+
+  getList() {
+    return this.http.get<any>('http://localhost:3000/api/getList');
+  }
+
+  getItem(productID) {
+    return this.http.post<any>('http://localhost:3000/api/getList', { 'productid':productID });
+  }
+
+  update(product:Products) {
+    return this.http.post<any>('http://localhost:3000/api/update', product );
+  }
+
+  delete(productID) {
+    return this.http.post<any>('http://localhost:3000/api/delete', { 'productid':productID });
+  }
+
+  //TODO - check if valid product id.
+
+
 }
