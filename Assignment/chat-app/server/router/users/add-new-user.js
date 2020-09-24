ef05a7e569d8860c25410ab8b6e4f8d7c559c189
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-25 02:46:27 +1000

    Added bcrypt password hash to new user accounts

--- Assignment/chat-app/server/router/users/add-new-user.js
+++ Assignment/chat-app/server/router/users/add-new-user.js
@@ -1,12 +1,16 @@
+const bcrypt = require('bcrypt');
+
 module.exports = function(usersCollection, app) {
 
     app.post('/api/add-new-user', (req, res) => {
 
-        console.log(req.body);
-        usersCollection.insertOne(req.body, (err, result) => {
-            if(err) { return res.sendStatus(400); }
-            console.log(result);
-            return res.sendStatus(200);
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+            usersCollection.insertOne({username: req.body.username, email: req.body.email, password: hash, role: req.body.role }, (err, result) => {
+                if(err) { return res.sendStatus(400); }
+                console.log(result);
+                return res.sendStatus(200);
+            });
         });
+
     });
 };
