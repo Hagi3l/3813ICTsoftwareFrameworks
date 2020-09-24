ae6fd74f4334f8dcaf1a7b422c3dcccc676efb71
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-24 14:52:48 +1000

    Removed debug code

--- Labs/week9/backend/App/update.js
+++ Labs/week9/backend/App/update.js
@@ -8,7 +8,6 @@ module.exports = (collection, app, ObjectId) => {
 
         product = req.body;
         var objectid = ObjectId(product.objid);
-        console.log(product);
         collection.updateOne( {_id:objectid}, { $set: {name:product.name, description:product.description, price: product.price, units:product.units} }, (err) => {
           if(err) { return res.sendStatus(400) }
           res.send({'ok':product.objid});
