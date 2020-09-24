3626b59b056ea3c4aa60885ec930ad833e1e5d33
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-24 15:17:29 +1000

    Updated seed schema

--- Labs/week9/backend/App/app.js
+++ Labs/week9/backend/App/app.js
@@ -1,32 +1,32 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const add = require('./add');
-const read = require('./read');
-const remove = require('./remove');
-const update = require('./update');
+// const add = require('./add');
+// const read = require('./read');
+// const remove = require('./remove');
+// const update = require('./update');
 
 const docArray = [
   {
-      "id": "1",
-      "name": "iPhone 6",
-      "description": "Best phone in the world!",
-      "price": 999,
-      "units": 99
+      id: 1,
+      name: "iPhone 6",
+      desc: "Best phone in the world!",
+      price: 999,
+      units: 99
   },
   {
-      "id": "2",
-      "name": "iPhone X",
-      "description": "Not a bad phone",
-      "price": 1499,
-      "units": 25
+      id: 2,
+      name: "iPhone X",
+      desc: "Not a bad phone",
+      price: 1499,
+      units: 25
   },
   {
-      "id": "3",
-      "name": "Samsung Galaxy S10",
-      "description": "Number 1 phone on the planet",
-      "price": 850,
-      "units": 75
+      id: 3,
+      name: "Samsung Galaxy S10",
+      desc: "Number 1 phone on the planet",
+      price: 850,
+      units: 75
   }
 ];
 
@@ -46,7 +46,9 @@ const client = MongoClient.connect(url, function(err, client) {
 
   const collection = db.collection('products');
 
-  add(collection, docArray);
+  //Seeding the database
+  db.collection('products').drop();
+  collection.insertMany(docArray);
 
   client.close();
 });
