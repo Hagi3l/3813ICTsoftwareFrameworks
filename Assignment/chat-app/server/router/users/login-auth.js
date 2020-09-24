73516f5f33f1151e7ac0c40abbfa24daef72e655
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-25 03:24:33 +1000

    login auth working
    todo: error handling

--- Assignment/chat-app/server/router/users/login-auth.js
+++ Assignment/chat-app/server/router/users/login-auth.js
@@ -1,4 +1,27 @@
+const bcrypt = require('bcrypt');
 
+module.exports = (usersCollection, app) => {
+
+    app.post('/api/login-auth', (req, res) => {
+
+        console.log(req.body);
+
+        usersCollection.find({ username: req.body.username }).toArray( (err, result) => {
+            if(err) { return res.sendStatus(400); }
+            console.log(result);
+            bcrypt.compare(req.body.password, result[0].password, (err, result) => {
+                if(err) { return res.sendStatus(400); }
+                if (result === true) {
+                    console.log("Login Approved!");
+                    return res.sendStatus(200);
+                } else {
+                    console.log("Invalid username or password");
+                    return res.send({status: 400, message: "Username or Password not found"});
+                }
+            });
+        });
+    });
+}
 
   //TODO:
     // check for user + pass in the collection
