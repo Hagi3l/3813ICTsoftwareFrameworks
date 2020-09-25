94afaf6f15bc11dd37f2996b72a481a68e98d947
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-25 15:04:35 +1000

    Changed return array for get-users
    - Removed passwords from the return array

--- Assignment/chat-app/server/router/users/get-users.js
+++ Assignment/chat-app/server/router/users/get-users.js
@@ -2,10 +2,9 @@ module.exports = function(usersCollection, app) {
 
     app.get('/api/get-users', (req, res) => {
 
-        console.log(req.body);
-        usersCollection.find({}).toArray( (err, result) => {
+        usersCollection.find({}, {projection: {password:0}}).toArray( (err, result) => {
             if(err) { return res.sendStatus(400); }
-            return res.status(200).send({ username: data[0].username, email: data[0].email, role: data[0].role });
+            return res.status(200).send(result);
         });
     });
 };
