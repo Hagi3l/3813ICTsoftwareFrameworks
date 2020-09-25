ff634ce050223a76935047dba7542e36d842f94a
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-25 13:19:11 +1000

    Updated error handling

--- Assignment/chat-app/server/router/users/add-new-user.js
+++ Assignment/chat-app/server/router/users/add-new-user.js
@@ -6,11 +6,14 @@ module.exports = function(usersCollection, app) {
 
         bcrypt.hash(req.body.password, 10, (err, hash) => {
             usersCollection.insertOne({username: req.body.username, email: req.body.email, password: hash, role: req.body.role }, (err, result) => {
-                if(err) { return res.sendStatus(400); }
-                console.log(result);
+                if(err) {
+                    if (err.code == 11000) {
+                        return res.status(400).send({ code: 10, message: "Username taken"});
+                    }
+                    return res.sendStatus(400);
+                }
                 return res.sendStatus(200);
             });
         });
-
     });
 };
