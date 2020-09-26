f857bbc830125ab7e9b46daa4db040676d179bb8
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-26 17:01:51 +1000

    Changed insert command

--- Assignment/chat-app/server/router/users/add-new-user.js
+++ Assignment/chat-app/server/router/users/add-new-user.js
@@ -5,7 +5,7 @@ module.exports = function(usersCollection, app) {
     app.post('/api/add-new-user', (req, res) => {
 
         bcrypt.hash(req.body.password, 10, (err, hash) => {
-            usersCollection.insertOne({username: req.body.username, email: req.body.email, password: hash, role: req.body.role }, (err, result) => {
+            usersCollection.insert({username: req.body.username, email: req.body.email, password: hash, role: req.body.role }, (err, result) => {
                 if(err) {
                     if (err.code == 11000) {
                         return res.status(400).send({ code: 10, message: "Username taken"});
