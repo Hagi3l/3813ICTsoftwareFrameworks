7722c2d0484543af1ebe535542d135d600459b08
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-26 17:02:45 +1000

    Added processing to arrays
    - The group_assistant and group_users are processed to convert id from string to an ObjectId.

--- Assignment/chat-app/server/router/groups/add-group.js
+++ Assignment/chat-app/server/router/groups/add-group.js
@@ -2,12 +2,20 @@ module.exports = function(usersCollection, groupsCollection, app, ObjectId) {
 
     app.post('/api/add-new-group', (req, res) => {
 
-        let objectid = ObjectId(req.body.id);
+        let objectid = ObjectId(req.body.user_id);
+        let ga = [];
+        req.body.group_assistants.forEach(assistant => {
+            ga.push(ObjectId(assistant));
+        });
+        let gu = [];
+        req.body.group_users.forEach(user => {
+            gu.push(ObjectId(user));
+        });
 
         usersCollection.findOne({_id: objectid}, (err, data) => {
             if (data.role == "super-admin" || data.role == "group-admin") {
                 console.log('added by a super-admin or group-admin');
-                groupsCollection.insertOne({group_name: req.body.group_name, group_admins: ["super-admin", "group-admin"],  group_users: [] }, (err, result) => {
+                groupsCollection.insertOne({group_name: req.body.group_name, group_admins: ["super-admin", "group-admin"], group_assistants: ga, group_users: gu }, (err, result) => {
                     if(err) {return res.sendStatus(400);}
                     return res.sendStatus(200);
                 });
