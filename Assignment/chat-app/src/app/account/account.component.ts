219df6225f0c1b9b6a21527b83decc71f1da5809
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-27 19:53:51 +1000

    Retrieve data from user-data service
    - Get user info

--- Assignment/chat-app/src/app/account/account.component.ts
+++ Assignment/chat-app/src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserDataService } from '../services/user-data.service';
 
 @Component({
   selector: 'app-account',
@@ -8,16 +9,18 @@ import { Router } from '@angular/router';
 })
 export class AccountComponent implements OnInit {
 
-  constructor(private router: Router) { }
+    constructor(private router: Router, private userService: UserDataService) { }
 
-  active_user:boolean = false;
+    active_user:boolean = false;
+    user_data;
 
-  ngOnInit() {
-    if ("active-user" in localStorage) {
-        this.active_user = true;
-    } else {
-        this.router.navigateByUrl('');
+    ngOnInit() {
+        if ("active-user" in localStorage) {
+            this.active_user = true;
+            this.user_data = this.userService.get_user_info();
+            console.log(this.user_data);
+        } else {
+            this.router.navigateByUrl('');
+        }
     }
-  }
-
 }
