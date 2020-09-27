016c84ca326be80a20cfb4a717a2349a3b7133d5
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-27 17:32:19 +1000

    Modified nav design & added user login check

--- Assignment/chat-app/src/app/navbar/navbar.component.ts
+++ Assignment/chat-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router) { }
+
+  active_user:boolean = false;
+
+  ngOnInit() {
+    if ("active-user" in localStorage) {
+        this.active_user = true;
+    }
   }
 }
