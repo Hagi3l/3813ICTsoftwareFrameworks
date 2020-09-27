b61befb1afb0969132eaed84469c277940e3c30b
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-28 03:17:58 +1000

    Modified get channel request
    - Ability to pass data through get request

--- Assignment/chat-app/server/router/channels/get-channels.js
+++ Assignment/chat-app/server/router/channels/get-channels.js
@@ -1,7 +1,8 @@
 module.exports = function(channelsCollection, app) {
 
-    app.get('/api/get-channels', (req, res) => {
-
+    app.get('/api/get-channels/:data', (req, res) => {
+        const data = req.params.data;
+        console.log(data);
         channelsCollection.find({}).toArray( (err, result) => {
             if(err) { return res.sendStatus(400); }
             return res.send(result);
--- Assignment/chat-app/src/app/services/group-channel.service.ts
+++ Assignment/chat-app/src/app/services/group-channel.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupChannelService {
 
-  constructor() { }
+    constructor(private router: Router, private httpClient: HttpClient) { }
+
+    public fetchGroupData(): Observable<any> {
+        return this.httpClient.get('/api/get-groups');
+    }
+
+    public fetchChannelData(groupId): Observable<any> {
+        console.log('hello');
+        return this.httpClient.get('/api/get-channels/' + groupId);
+    }
+
 }
