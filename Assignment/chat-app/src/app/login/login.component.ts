7618501c8dd2a09ef9922e168a83b547b5806ba2
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-28 03:18:39 +1000

    Updated request method

--- Assignment/chat-app/src/app/login/login.component.ts
+++ Assignment/chat-app/src/app/login/login.component.ts
@@ -4,15 +4,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UserDataService } from '../services/user-data.service';
 
 
-const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json', observe: 'body'}) };
-const BACKEND_URL = 'http://localhost:3000';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
 
     constructor(private router: Router, private httpClient: HttpClient, private userService: UserDataService) {}
@@ -30,7 +26,7 @@ export class LoginComponent implements OnInit {
     }
 
     login() {
-        this.httpClient.post(BACKEND_URL + '/api/login-auth', this.loginDetails, httpOptions).subscribe((data: any) => {
+        this.httpClient.post('/api/login-auth', this.loginDetails).subscribe((data: any) => {
             if (data.code == 1 || data.code == 2) {
                 console.log('error');
                 // display error in login field
