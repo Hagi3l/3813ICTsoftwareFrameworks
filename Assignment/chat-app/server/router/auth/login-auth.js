f50fe4245599a795407d729a2baa4d4814ad5dad
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-27 16:36:52 +1000

    Modified login component
    - Added better error handling
    - Adjusted form design
    - Adjusted http response codes

--- Assignment/chat-app/server/router/auth/login-auth.js
+++ Assignment/chat-app/server/router/auth/login-auth.js
@@ -7,14 +7,14 @@ module.exports = (usersCollection, app) => {
         usersCollection.findOne({ username: req.body.username }, (err, data)  => {
             if(err) { return res.sendStatus(400); }
             if(data === null) {
-                return res.status(400).send({message: "User not found"});
+                return res.status(200).send({ code: 1, message: "User not found"});
             }
             bcrypt.compare(req.body.password, data.password, (err, result) => {
                 if(err) { return res.sendStatus(400); }
                 if (result === true) {
                     return res.status(200).send({ username: data.username, email: data.email, role: data.role });
                 } else {
-                    return res.status(400).send({ message: "Password does not match"});
+                    return res.status(200).send({ code: 2, message: "Password does not match"});
                 }
             });
         });
--- Assignment/chat-app/src/app/login/login.component.ts
+++ Assignment/chat-app/src/app/login/login.component.ts
@@ -2,11 +2,7 @@ import { Component,  OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-};
-
-
+const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json', observe: 'body'}) };
 const BACKEND_URL = 'http://localhost:3000';
 
 @Component({
@@ -17,32 +13,27 @@ const BACKEND_URL = 'http://localhost:3000';
 
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private httpClient: HttpClient) {}
+    constructor(private router: Router, private httpClient: HttpClient) {}
 
-  username: string;
-  password: string;
-  session: boolean;
+    username: string;
+    password: string;
+    session: boolean;
 
-  loginDetails = {username: this.username, password: this.password};
+    loginDetails = null;
 
-  ngOnInit() {
+    ngOnInit() {
+        this.loginDetails = {username: this.username, password: this.password};
+    }
 
-  }
-
-  public loginfunc() {
-
-    console.log(this.loginDetails);
-    this.httpClient.post(BACKEND_URL + '/api/login-auth', this.loginDetails, httpOptions)
-    .subscribe((data: any) => {
-      if (data.ok) {
-        localStorage.setItem('active-user', data.toString());
-        // this.httpClient.post(BACKEND_URL + '/api/login-success', data, httpOptions)
-        // .subscribe((m: any) => {});
-        this.router.navigateByUrl('account');
-      } else {
-        alert('Sorry, invalid username or password');
-      }
-
-    });
-  }
+    login() {
+        this.httpClient.post(BACKEND_URL + '/api/login-auth', this.loginDetails, httpOptions).subscribe((data: any) => {
+            if (data.code == 1 || data.code == 2) {
+                console.log('error');
+                // display error in login field
+            } else {
+                localStorage.setItem('active-user', data.toString());
+                this.router.navigateByUrl('account');
+            }
+        });
+    }
 }
