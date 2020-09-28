e3030cdb2ea62575b97ff21bc871d286da8dd898
V <30593374+Hagi3l@users.noreply.github.com>
2020-09-28 17:32:29 +1000

    renamed parama id

--- Assignment/chat-app/server/router/channels/get-channels.js
+++ Assignment/chat-app/server/router/channels/get-channels.js
@@ -1,7 +1,7 @@
 module.exports = function(channelsCollection, app, ObjectId) {
 
-    app.get('/api/get-channels/:data', (req, res) => {
-        let objectid = ObjectId(req.params.data);
+    app.get('/api/get-channels/:group_id', (req, res) => {
+        let objectid = ObjectId(req.params.group_id);
         channelsCollection.find({group_id: objectid}).toArray( (err, result) => {
             if(err) { return res.sendStatus(400); }
             return res.send(result);
