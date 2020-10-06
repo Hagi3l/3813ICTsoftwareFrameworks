8c7a5dcd08b6b4b06f826b7436037dd47aec28f1
V <30593374+Hagi3l@users.noreply.github.com>
2020-10-06 20:45:31 +1000

    Update chat.component.ts
    - Modified get groups
    - Modified get channels
    - Checks users role against groups and channels
    - Checks if user is in group and channel

--- Assignment/chat-app/src/app/chat/chat.component.ts
+++ Assignment/chat-app/src/app/chat/chat.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../services/socket.service';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserDataService } from '../services/user-data.service';
+
 
 const BACKEND_URL = 'http://localhost:3000';
 const httpOptions = {
@@ -17,8 +19,8 @@ export class ChatComponent implements OnInit {
 
     newMessage:String;
     messages:Array<{username:String, message:String}> = [];
-    groups:Array<{id:Number, group_name:String, users:Array<String>}> = [];
-    group:Array<{id:Number, group_name:String, users:Array<String>}>;
+    // groups:Array<{id:Number, group_name:String, users:Array<String>}> = [];
+
     channels:Array<{id:Number, channel_name:String, group_id:Number, users:Array<String>}> = [];
     channel:String;
     ioConnection:any;
@@ -26,69 +28,94 @@ export class ChatComponent implements OnInit {
     username = localStorage.getItem('username');
     session: boolean;
 
-    constructor(private socketService:SocketService, private router: Router, private httpClient: HttpClient) { }
+    constructor(private socketService:SocketService, private router: Router, private httpClient: HttpClient, private userService: UserDataService) { }
+
+    private active_user: boolean;
+    private active_user_details: any;
+    private active_user_group_assistant: boolean = false;
+
+    public groups = [];
+    public group_selected: any;
 
     ngOnInit() {
 
-      try {
-          this.username;
-          }
-        catch(err){
-          alert ("Please login");
-          this.router.navigateByUrl('/login');
+        this.active_user = this.userService.active_user;
+        if (this.active_user && this.userService.roles.includes(this.userService.user_info.role) ) {
+            this.active_user_details = this.userService.user_info;
+            console.log(this.active_user_details);
+        } else {
+            this.router.navigateByUrl('');
         }
 
+    //   try {
+    //       this.username;
+    //       }
+    //     catch(err){
+    //       alert ("Please login");
+    //       this.router.navigateByUrl('/login');
+    //     }
+
         this.getGroups();
-        this.initIoConnection();
+        // this.initIoConnection();
     }
 
     private initIoConnection() {
         this.socketService.initSocket();
-        this.socketService.onMessage()
-        .subscribe(data => this.messages.push(data));
+        this.socketService.onMessage().subscribe(data => this.messages.push(data));
 
-        this.socketService.newUserJoined()
-        .subscribe(data => this.messages.push(data));
+        this.socketService.newUserJoined().subscribe(data => this.messages.push(data));
 
-        this.socketService.userLeftRoom()
-        .subscribe(data => this.messages.push(data));
+        this.socketService.userLeftRoom().subscribe(data => this.messages.push(data));
 
         console.log(this.messages);
     }
 
     private getGroups() {
-
-        this.httpClient.post(BACKEND_URL + '/api/groups', httpOptions)
-        .subscribe((data: any) => {
-            if (data) {
-                data.forEach(g => {
-                    g.users.forEach(user => {
-                        if (user == this.username) {
-                            this.groups.push(g);
-                        }
-                    });
-                });
+        this.groups = [];
+        this.httpClient.get(BACKEND_URL + '/api/get-groups', httpOptions).subscribe((data: any) => {
+            if (this.active_user_details.role == 'super-admin' || this.active_user_details.role == 'group-admin') {
+                this.groups = data;
             } else {
-                alert('No groups, please add some in the admin panel');
+                data.forEach(group => {
+                    console.log(group.group_users);
+                    const exists = Boolean(group.group_assistants.find(x => x === this.active_user_details._id));
+                    const exists2 = Boolean(group.group_users.find(x => x === this.active_user_details._id));
+                    if (exists || exists2) {
+                        exists ? this.active_user_group_assistant = true : this.active_user_group_assistant = false;
+                        this.groups.push(group);
+                    } else {
+                        console.log('not in any groups');
+                    }
+                });
             }
+
+            // if (exists || user.role == "super-admin" || user.role == "group-admin") {
+            //     console.log('found user');
+            //     // Display an error to the admin
+            // } else {
+            //     this.groupAssistants.push(user);
+            // }
 
         });
     }
 
-    private getChannnels(group_id) {
-
-        this.httpClient.post(BACKEND_URL + '/api/channels', httpOptions)
-        .subscribe((data: any) => {
-            if (data) {
-                data.forEach(c => {
-                    if (c.group_id == group_id) {
-                        this.channels.push(c);
+    private getChannnels(group) {
+        this.channels = [];
+        this.httpClient.get(BACKEND_URL + '/api/get-channels/' + group._id, httpOptions).subscribe((data: any) => {
+            if (this.active_user_group_assistant || this.active_user_details.role == 'super-admin' || this.active_user_details.role == 'group-admin') {
+                this.channels = data;
+            } else {
+                data.forEach(channel => {
+                    console.log(channel);
+                    const exists = Boolean(channel.channel_users.find(x => x === this.active_user_details._id));
+                    if (exists) {
+                        this.channels.push(channel);
+                    } else {
+                        console.log('not in any channels');
                     }
                 });
-            } else {
-                alert('No channels available, please add some in the admin panel');
             }
-
+            // console.log(data);
         });
     }
 
@@ -113,9 +140,7 @@ export class ChatComponent implements OnInit {
         this.socketService.leaveRoom({username: this.username, room: this.channel});
     }
 
-    public groupSelected() {
-        this.channels = [];
-        this.getChannnels(this.group);
-
+    public groupSelected(group) {
+        this.getChannnels(group);
     }
 }
