d8f432d3cb271bdcf9a124aa8e14fcbec6ab370d
V <30593374+Hagi3l@users.noreply.github.com>
2020-10-08 03:27:51 +1000

    Update test.js

--- Assignment/chat-app/server/unitTest/test.js
+++ Assignment/chat-app/server/unitTest/test.js
@@ -1,49 +1,63 @@
-var assert = require('assert');
-var http = require('http');
+let assert = require('assert');
+let app = 'http://localhost:3000/';
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
 
-var url = 'http://localhost:3000/';
 
 describe('Server API testing', function() {
 
     before(() => { console.log('before test'); });
     after(() => { console.log('after test'); });
 
-    describe('api/users (get)', () => {
-        it('should return all users', () => {
-            http.get(url + 'api/users', (res) => {
-
-                assert.strictEqual(res.statusCode, 200);
-                assert.strictEqual(res.statusMessage, 'OK');
-                res.on('end', () => {
-                    assert.strictEqual(body, body.exists());
-                });
-
+    describe('testing getting users - api/users (get)', () => {
+            it('should return all users', (done) => {
+            chai.request(app)
+                .get('api/users')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
             });
         });
     });
 
-    describe('api/get-groups (get)', () => {
-        it('should return all groups', () => {
-            http.get(url + 'api/get-groups', (res) => {
-
-                assert.strictEqual(res.statusCode, 200);
-                assert.strictEqual(res.statusMessage, 'OK');
-                res.on('end', () => {
-                    assert.strictEqual(body, body.exists());
-                });
+    describe('testing getting groups - api/get-groups (get)', () => {
+        it('should return all groups', (done) => {
+            chai.request(app)
+                .get('api/get-groups')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
             });
         });
     });
 
-    describe('api/get-channels/ (get)', () => {
-        it('should return all channels in the group', () => {
+    describe('testing getting channels - api/get-channels/ (get)', () => {
+        it('should return all channels in the group', (done) => {
             var group_id = '5f7005865006b8453028d4cd';
-            http.get(url + 'api/get-channels/'+group_id, (res) => {
-                assert.strictEqual(res.statusCode, 200);
-                assert.strictEqual(res.statusMessage, 'OK');
-                res.on('end', () => {
-                    assert.strictEqual(body[0].group_id, group_id);
-                });
+            chai.request(app)
+                .get('api/get-channels/'+group_id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    done();
+            });
+        });
+    });
+
+    describe('testing login auth - api/login-auth/ (post)', () => {
+        it('should fail due to hashing', (done) => {
+            chai.request(app)
+                .post('api/login-auth')
+                .type('form')
+                .send({"username": "rand", "password": "rand" })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
             });
         });
     });
